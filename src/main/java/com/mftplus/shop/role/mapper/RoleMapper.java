c8b5537eea1c19bdc971c40c2e4b4806db2a0a9b
MahdiarMansouri <mahdiarm60@gmail.com>
2025-04-19 18:21:52 +0330

    BaseServiceImpl Update

--- src/main/java/com/mftplus/shop/role/mapper/RoleMapper.java
+++ src/main/java/com/mftplus/shop/role/mapper/RoleMapper.java
@@ -5,11 +5,7 @@ import com.mftplus.shop.config.CentralMapperConfig;
 import com.mftplus.shop.mapper.BaseMapper;
 import com.mftplus.shop.role.Role;
 import com.mftplus.shop.role.dto.RoleDto;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.io.ObjectInputFilter;
 
 @Mapper(config = CentralMapperConfig.class)
 public interface RoleMapper extends BaseMapper<Role, RoleDto> {
--- src/main/java/com/mftplus/shop/user/service/UserSevice.java
+++ src/main/java/com/mftplus/shop/user/service/UserSevice.java
@@ -4,6 +4,7 @@ import com.mftplus.shop.mapper.BaseMapper;
 import com.mftplus.shop.service.BaseServiceImpl;
 import com.mftplus.shop.user.User;
 import com.mftplus.shop.user.dto.UserDto;
+import com.mftplus.shop.user.mapper.UserMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
